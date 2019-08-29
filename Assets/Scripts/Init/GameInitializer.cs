a8f5b152b993f55eff251582bad1c3e7d7a5d473
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-08-29 18:35:40 +0200

    Import Photon Bolt and samples

---
+++ Assets/Scripts/Init/GameInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    void Awake() {
+        joinServer();
+        setPlayerPrefs();
+    }
+
+    private int findServer() {
+        return 1;
+    }
+
+    private void joinServer() {
+        int server = findServer();
+    }
+
+    private void setPlayerPrefs() {
+
+    }
+}
