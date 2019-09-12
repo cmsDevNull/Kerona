e530fc5672c6a968cc0bc8f64199c8adeb1e18fd
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-09-12 18:55:10 +0200

    Create prototypical character creation scene

---
+++ Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameInitializer : MonoBehaviour
+{
+    public string setupSceneName;
+
+    void Awake() {
+        setPlayerPrefs();
+        if (!(PlayerPrefs.GetInt("setupCompleted") == 1)) SceneManager.LoadScene(setupSceneName);
+    }
+
+    private void setPlayerPrefs() {
+
+    }
+}
