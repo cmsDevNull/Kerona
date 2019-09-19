7f9dfb55596b4e82c9f5c0de544ad1cba965fc37
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-09-19 20:55:47 +0200

    Create FirstStart scene

---
+++ Assets/Scripts/FirstStart/BrightnessController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BrightnessController : MonoBehaviour
+{
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) decreaseBrightness();       
+        if (Input.GetKeyDown(KeyCode.RightArrow)) increaseBrightness();       
+    }
+
+    public void decreaseBrightness() {
+        this.gameObject.transform.Rotate(0, 0, 5f);
+        Screen.brightness -= 0.1f;
+    }
+
+    public void increaseBrightness() {
+        this.gameObject.transform.Rotate(0, 0, -5f);
+        Screen.brightness += 0.1f;
+    }
+}
