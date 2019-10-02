ac870040f556d280845bca304000f157c915e7c3
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-10-03 00:19:54 +0200

    Update dungeon generator logic

--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Dungeon
+﻿public class Dungeon
 {
     public DungeonRoom[][][] rooms;
 }
 
 public class DungeonRoom
 {
+    public int roomWidth;
+    public int roomLength;
+    public int roomHeight;
+    public int roomNumber;
     public Coordinate entrancePoint;
-    public Coordinate exitPoint;
+    public Coordinate[] exitPoints;
+    public DungeonEvent[] events;
 }
 
 public class Coordinate
@@ -18,4 +19,9 @@ public class Coordinate
     public int x;
     public int y;
     public int z;
+}
+
+public class DungeonEvent
+{
+    // a function that is called after a certain requirement is fulfilled
 }
