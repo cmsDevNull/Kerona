8391c70f78a727f79e6006b8b8f67d1d0b0b650f
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-10-24 18:36:29 +0200

    Implement room placing functionality

--- Assets/Scripts/Infrastructure/DungeonRoom.cs
+++ Assets/Scripts/Infrastructure/DungeonRoom.cs
@@ -2,13 +2,9 @@
 
 public class DungeonRoom : MonoBehaviour
 {
-    public int roomWidth;
-    public int roomLength;
-    public int roomHeight;
     public RoomType type;
-    public Vector3 entrancePoint;
     public Vector3[] exitPointPositions;
-    public Quaternion[] exitPointRotations;
+    public Vector3[] exitPointRotations;
     public DungeonEvent[] events;
 }
 
--- Assets/Scripts/TestBuilder.cs
+++ Assets/Scripts/TestBuilder.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 /*
 Dungeon Building Algorithm 
     1. calculate scaled depth 
-    2. initiate empty hypermatrix 
-    3. place entrance 
-    4. place arena or empty room until depth = 0 
-    5. for every exit left, place treasure, special or dead-end room 
-    6. for the last exit, place boss room
+    2. place entrance 
+    3. place room with exits until depth = 0 
+    4. for every exit left, place room without exit
+    5. for the last exit, place boss room
 */
 public class TestBuilder : MonoBehaviour
 {
     public int players = 3;
     public int minDepth = 6;
     public DungeonType buildingAlgorithm;
+
     List<Vector3> globalExitPointPositions = new List<Vector3>();
-    List<Quaternion> globalExitPointRotations = new List<Quaternion>();
+    List<Vector3> globalExitPointRotations = new List<Vector3>();
 
     void Start() {
-        int scaledDepth = scaleDepth(minDepth, players);
-
         Object[] res = Resources.LoadAll("");
 
-        for (int i = 0; i < scaledDepth; i++) {
-            
+        int scaledDepth = scaleDepth(minDepth, players);
+        for (int i = 0; i < scaledDepth; i++) {    
             GameObject room = pickRoom(res, i);
-
             placeRoom(room);
         }
     }
@@ -56,19 +53,70 @@ public class TestBuilder : MonoBehaviour
         List<GameObject> rooms = new List<GameObject>();
         foreach (var r in res) if (r.name.Contains(roomName)) rooms.Add((GameObject)r);
         GameObject room = rooms[Random.Range(0, rooms.Count)];
-        addExitPoint(room.GetComponent<DungeonRoom>());
         
         return room;
     }
 
-    private void placeRoom(GameObject room){
-        Instantiate(room);
+    private void placeRoom(GameObject newRoom) {
+        // TODO: adjust distances
+        Vector3 nextMove = getVector();
+        nextMove = overkill(nextMove, newRoom);
+        newRoom.transform.position = cullFirstFromList(globalExitPointPositions);
+        newRoom.transform.Rotate(cullFirstFromList(globalExitPointRotations));
+        newRoom.transform.position += nextMove;
+        Instantiate(newRoom);
+        addExitPoints(newRoom.GetComponent<DungeonRoom>());
     }
 
-    private void addExitPoint(DungeonRoom room) {
-        foreach (Vector3 epp in room.exitPointPositions) globalExitPointPositions.Add(epp);
-        foreach (Quaternion epr in room.exitPointRotations) globalExitPointRotations.Add(epr);        
+    private Vector3 getVector() { 
+        try {
+            return globalExitPointPositions[0]; 
+        } catch (System.Exception) {
+            return new Vector3(0, 0, 0);
+        }
     }
+
+    private Vector3 overkill(Vector3 epp, GameObject room){
+        // TODO: room is currently previous room, not new room
+        // also, refactor
+        if (epp.x != 0) {
+            float x = room.transform.localScale.x / 2;
+            epp += new Vector3(x, 0, 0);
+        }
+        if (epp.y != 0) {
+            float y = room.transform.localScale.y / 2;
+            epp += new Vector3(0, y, 0);
+        }
+        if (epp.z != 0) {
+            float z = room.transform.localScale.z / 2;
+            epp += new Vector3(0, 0, z);
+        }
+        return epp;
+    }
+
+    private Vector3 cullFirstFromList(List<Vector3> list){
+        try {
+            Vector3 firstVal = list[0];
+            list.RemoveAt(0);
+            return firstVal;
+        } catch (System.Exception) {
+            return new Vector3(0, 0, 0);
+        }
+    }
+    
+    private void addExitPoints(DungeonRoom room) {
+        Vector3 roomPos = room.gameObject.transform.position;
+        Vector3 roomRot = room.gameObject.transform.rotation.eulerAngles;
+        foreach (Vector3 epp in room.exitPointPositions) globalExitPointPositions.Add(epp + roomPos);
+        foreach (Vector3 epr in room.exitPointRotations) globalExitPointRotations.Add(epr + roomRot);
+//        shuffleExitPoints();        
+    }
+/* 
+    private void shuffleExitPoints() {
+        globalExitPointPositions.Reverse();
+        globalExitPointRotations.Reverse();
+    }
+*/
 }
 
 public enum DungeonType
