56bd31cdebe7cce80ea4df693c232ab04900a026
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-12-19 20:28:32 +0100

    Implement ememy spawn events

---
+++ Assets/Scripts/TestEnemySpawnEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TestEnemySpawnEvent : MonoBehaviour
+{
+    public GameObject enemies;
+
+    void Start() => enemies.SetActive(false);
+
+    void OnTriggerEnter(Collider other) {
+        enemies.SetActive(true);
+        Destroy(this.gameObject);
+    }
+}
