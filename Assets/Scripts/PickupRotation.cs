6c590678d1b5705a5b0f49aea8e1bb27db7f5aae
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-03-26 12:59:11 +0100

    Create healthbar test scene, implement shield pickup

---
+++ Assets/Scripts/PickupRotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupRotation : MonoBehaviour
+{
+    public float rotationSpeed = 0.2f;
+
+    void Update() => transform.Rotate(new Vector3(0, rotationSpeed, 0));
+}
---
+++ Assets/Scripts/PickupShield.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class PickupShield : MonoBehaviour
+{
+    void OnTriggerEnter(Collider c){
+        Destroy(this.gameObject);
+    }
+}
