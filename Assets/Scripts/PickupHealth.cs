1c20f0f8f013602e6ea8b876939159277e71c824
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-04-02 14:57:40 +0200

    Implement health pickup

--- Assets/Scripts/PickupHealth.cs
+++ Assets/Scripts/PickupHealth.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class PickupShield : MonoBehaviour
+public class PickupHealth : MonoBehaviour
 {
     void OnTriggerEnter(Collider c){
         Destroy(transform.parent.gameObject);
