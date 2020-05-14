5822465017ec77b0127ab1ccdadb7cc225a171c0
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-05-14 20:28:38 +0200

    Implement animation

---
+++ Assets/Scripts/Healthbar/HealthbarFlameSpriteController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class HealthbarFlameSpriteController : MonoBehaviour
+{
+    public int animationSpeed = 18;
+    public List<Sprite> sprites;
+
+    private int timer;
+    private int spriteIndex;
+
+    void Start() {
+        spriteIndex = 0;
+        timer = animationSpeed;
+    }
+
+    void Update() {
+        switch (timer) {
+            case 0:
+                GetComponent<SpriteRenderer>().sprite = sprites[spriteIndex];
+                spriteIndex++;
+                if (spriteIndex >= sprites.Count) spriteIndex = 0;
+                timer = animationSpeed;
+                break;
+            default:
+                timer--;
+                break;
+        }
+    }
+}
