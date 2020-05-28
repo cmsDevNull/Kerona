a2ebe4cd1a606188067626c47c8336e9d2eed0ab
haringma15 <martin.haring2@edu.fh-joanneum.at>
2020-05-28 20:49:00 +0200

    Link health and player

--- Assets/Scripts/Healthbar/HealthbarOverlayController.cs
+++ Assets/Scripts/Healthbar/HealthbarOverlayController.cs
@@ -3,14 +3,16 @@ using UnityEngine.UI;
 
 public class HealthbarOverlayController : MonoBehaviour
 {
+    public TestPlayerHealth player;
+    public TextMesh healthText;
+
     public Image overlayFull; // 100%; healthy state.
     public Image overlayGood; // 75%; good state.
     public Image overlayBad; // 50%; worrying state.
     public Image overlayCritical; // 25%; critical state.
     public Image overlayDying; // 0%; death.
 
-    void Update()
-    {
-        
+    void Update() {
+        healthText.text = player.localHealth.ToString();
     }
 }
